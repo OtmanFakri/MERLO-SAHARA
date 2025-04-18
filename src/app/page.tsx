0ded3e5f99b8b07648b8004b1740b7e0b7d49338
Otman Fakri <otmanfakri2@gmail.com>
2025-04-18 19:19:36 +0100

    feat: Deatil Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,8 @@
-import {HeaderSection} from "@/app/components/HeaderSection/HeaderSection";
 import {EquipmentListingSection} from "@/app/components/EquipmentListingSection/EquipmentListingSection";
-import {SearchFilterSection} from "@/app/components/SearchFilterSection";
-import {FooterSection} from "@/app/components/FooterSection/FooterSection";
 
 export default function Home() {
   return (
       <div className="flex flex-col w-full min-w-80 max-w-[1920px] min-h-screen mx-auto bg-bananistyle">
-        <HeaderSection/>
         <EquipmentListingSection/>
-        <SearchFilterSection/>
-        <FooterSection/>
       </div>);
 }
